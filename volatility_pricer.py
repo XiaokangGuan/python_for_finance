011bb91fd81736678abb3228fd144923ae9ff0aa
Xiaokang Guan <xiaokang.guan@gmail.com>
2018-06-30 11:34:22 +0800

    First version / template of vol pricer.

---
+++ volatility_pricer.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+class VolatilityPricer():
+
+    def __init__(self):
+        self.data = None
+
+    def getRealizedVol(self):
+        """ Calculate the realized volatility from historical market data """
+        pass
+
+    def getImpliedVol(self):
+        """ Calculate the implied volatility from option market price """
+        pass
